78f5b1e6fdcce863e42be882e2fcddf5501a4de2
Ihr Name auf github <Ihre E-Mail von github>
2024-05-21 14:05:04 +0200

    Aufgabe 4 anfang

--- Vorlesung_4/funktions.py
+++ Vorlesung_4/funktions.py
@@ -1,34 +1,30 @@
 import pandas as pd
-import numpy as np
-
-
-def load_data():
-
-    # Use numpy.genfromtxt to read the CSV data from the file into a NumPy array
-    data_array = np.genfromtxt("../data/activity.csv", delimiter=',', dtype=None, names=True)
-
-    # Transpose the array to have each column in a separate NumPy array
-    column_names = data_array.dtype.names
-    column_arrays = {column: data_array[column] for column in column_names}
-
-
-    return column_arrays
-
 
 def load_activity(path="../data/activity.csv"):
     df = pd.read_csv(path)
     return df
 
 
+def find_best_effort (df, time_window, frequenz = 1):
+    df['RollingMean'] = df['PowerOriginal'].rolling(window = time_window).max()
+
+    df_best_effort = df['RollingMean']
+    # df_best_effort ["Größe Time Window"] = 
+    max_value = df['RollingMean'].max()
+    print (max_value)
+
+    return df_best_effort
+
+
+
 if __name__ == '__main__':
 
-    # print (load_data())
-    # print (read_my_csv())
-
-    # array = load_data()
-
     df = load_activity()
-    print(df.head())
+    # print(df.head())
 
-    power_original = df['PowerOriginal']
-    # print(power_original)
+    df['RollingMean'] = df['PowerOriginal'].rolling(window=3).max()
+    # print(df.head())
+
+    print (find_best_effort(df, 2))
+
+
