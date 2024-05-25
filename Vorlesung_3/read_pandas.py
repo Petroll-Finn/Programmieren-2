b70d56761d4bbe158bb5ee6d79f9650839c916e0
TillSchroeter <st7249@mci4me.at>
2024-05-25 10:55:44 +0200

    update

--- Vorlesung_3/read_pandas.py
+++ Vorlesung_3/read_pandas.py
@@ -22,7 +22,6 @@ def mittelwert (df):
 def max_Leistung (df):
     max_Leistung = df['PowerOriginal'].max()
     #print(max_Leistung)
-
     return max_Leistung
 
 def time():
@@ -31,17 +30,53 @@ def time():
     time_points = np.linspace(0, total_seconds, points)
     return (time_points)
 
+def max_Herzfrequenz (df):
+    max_Herzfrequenz = df["HeartRate"].max()
+    # print(max_Herzfrequenz)
+    return max_Herzfrequenz
+
+
+
 
 def make_plot_EKG (df):
+    
+    
     fig = px.line(df, x=time(), y=[df['PowerOriginal'], df['HeartRate']], title='Leistung und Herzfrequenz über die Zeit', labels={'x': 'Zeit [s]', 'value': 'Leistung[w] / Heart Rate[Bpm]'})
-    # fig.show()
+    
+    fig.add_shape(
+    type="rect",
+    x0=0, x1=1805,  # Zeit in Sekunden
+    y0=0, y1=1,  # Y-Achsenbereich, um den gesamten Diagramm zu überdecken
+    xref='x', yref='paper',
+    fillcolor="Brown",
+    opacity=1,
+    layer="below",
+    line_width=0,
+    )
+
+    fig.add_shape(
+    type="rect",
+    x0=0, x1=1805,  # Zeit in Sekunden
+    y0=0.1, y1=0.3,  # Y-Achsenbereich, um den gesamten Diagramm zu überdecken
+    xref='x', yref='paper',
+    fillcolor="aqua",
+    opacity=1,
+    layer="below",
+    line_width=0,
+    )
+    
+    fig.show()
     return fig
 
 
+
+
+
 if __name__ == '__main__':
-    print ("nochmal Hallo")
+    
     df = load_activity()
-    print_hallo()
+    print (max_Herzfrequenz(df))
+
     print (mittelwert(df))
     print (max_Leistung (df))
     make_plot_EKG(df)
